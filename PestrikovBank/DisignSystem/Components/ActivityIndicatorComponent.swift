6cf7c7e8b40e4762836333c04bb64c132851b6a0
Makar A. Pestrikov <mpxx088@gmail.com>
2025-05-21 15:55:53 +0300

    ds components

--- PestrikovBank/DisignSystem/Components/ActivityIndicatorComponent.swift
+++ PestrikovBank/DisignSystem/Components/ActivityIndicatorComponent.swift
@@ -11,42 +11,38 @@ import Combine
 public struct ActivityIndicatorComponentConfig {
     let id: String
     let style: UIActivityIndicatorView.Style
-    let onDarkColor: UIColor
-    let onLightColor: UIColor
+    let color: ColorVariant
 }
 
-//public class ActivityIndicatorComponent: Component {
-//    public var id: String
-//    public var view: UIView { indicatorView }
-//    private var indicatorView: UIActivityIndicatorView
-//    private var cancellables = Set<AnyCancellable>()
-//    private var config: ActivityIndicatorComponentConfig
-//    
-//    init(
-//        config: ActivityIndicatorComponentConfig
-//    ) {
-//        self.id = config.id
-//        
-//        let indicator = UIActivityIndicatorView(style: config.style)
-//        if UIScreen.main.traitCollection.userInterfaceStyle == .dark {
-//            indicator.tintColor = config.onDarkColor
-//        } else {
-//            indicator.color = config.onLightColor
-//        }
-//        self.indicatorView = indicator
-//        self.config = config
-//    }
-//    
-//    public func bind(to viewModel: AnyObject, of type: ViewModelType) {
-////        switch type {
-////        case .login:
-////            
-////        case .signUp:
-////            
-////        }
-//    }
-//    
-//    public func setupConstraints(in container: UIView, preset: ConstraintPreset) {
-//        setupConstraintsDefault(self, in: container, preset: preset)
-//    }
-//}
+public class ActivityIndicatorComponent: UIActivityIndicatorView, Component {
+    public var id: String
+    
+    init(
+        config: ActivityIndicatorComponentConfig
+    ) {
+        self.id = config.id
+        super.init(frame: .zero)
+        
+        switch config.color {
+        case .system: break
+        case .systemRed: color = .systemRed
+        case .systemBlue: color = .systemBlue
+        case .systemGreen: color = .systemGreen
+        case .systemOrange: color = .systemOrange
+        case .systemPurple: color = .systemPurple
+        case .single(let color): self.color = color
+        case .dynamic(onLight: let onLight, onDark: let onDark):
+            color = dynColor(onLight: onLight, onDark: onDark)
+        }
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public func bind(to viewModel: ViewModelType) {}
+    
+    public func setupConstraints(in container: UIView, preset: ConstraintPreset) {
+        setupConstraintsDefault(self, in: container, preset: preset)
+    }
+}
--- PestrikovBank/DisignSystem/Components/ButtonComponent.swift
+++ PestrikovBank/DisignSystem/Components/ButtonComponent.swift
@@ -16,41 +16,48 @@ public struct ButtonComponentConfig {
     let isEnabled: Bool
     let fontName: String
     let fontSize: CGFloat
-    let onDarkTextColor: UIColor
-    let onLightTextColor: UIColor
+    let color: ColorVariant
 }
 
-//public struct ButtonComponent: Component {
-//    public var id: String
-////    public var view: UIView { buttonView }
-//    private var buttonView: UIButton
-//    private var cancellables = Set<AnyCancellable>()
-//    private var config: ButtonComponentConfig
-//    
-//    init(
-//        config: ButtonComponentConfig
-//    ) {
-//        self.id = config.id
-//        
-//        let btn = UIButton(type: config.buttonType)
-//        btn.setTitle(config.title, for: config.uiControlState)
-//        btn.isEnabled = config.isEnabled
-//        
-//        btn.titleLabel!.font = UIFont(name: config.fontName, size: config.fontSize)
-//        buttonView = btn
-//        self.config = config
-//    }
-//    
-//    public func bind(to viewModel: AnyObject, of type: ViewModelType) {
-//        switch type {
-//        case .accounts:
-//            break   // unimplemented
-//        case .userDetails:
-//            break   // unimplemented
-//        }
-//    }
-//    
-//    public func setupConstraints(in container: UIView, preset: ConstraintPreset) {
-//        setupConstraintsDefault(self, in: container, preset: preset)
-//    }
-//}
+public class ButtonComponent: UIButton, Component {
+    public var id: String
+    
+    init(
+        config: ButtonComponentConfig
+    ) {
+        self.id = config.id
+        super.init(frame: .zero)
+        
+        self.setTitle(config.title, for: config.uiControlState)
+        self.isEnabled = config.isEnabled
+        
+        self.titleLabel!.font = UIFont(name: config.fontName, size: config.fontSize)
+        
+        switch config.color {
+        case .system: break
+        case .systemRed: tintColor = .systemRed
+        case .systemBlue: tintColor = .systemBlue
+        case .systemGreen: tintColor = .systemGreen
+        case .systemOrange: tintColor = .systemOrange
+        case .systemPurple: tintColor = .systemPurple
+        case .single(let color): tintColor = color
+        case .dynamic(onLight: let onLight, onDark: let onDark):
+            tintColor = dynColor(onLight: onLight, onDark: onDark)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public func bind(to viewModel: ViewModelType) {
+        switch viewModel {
+        case .accountsViewModel(_):
+            break
+        }
+    }
+    
+    public func setupConstraints(in container: UIView, preset: ConstraintPreset) {
+        setupConstraintsDefault(self, in: container, preset: preset)
+    }
+}
